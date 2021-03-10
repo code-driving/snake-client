98eff0226a4b7e7389b9be372f56a7c041fc40c3
code-driving <tittan7@gmail.com>
2021-03-10 19:48:52 +0000

    creating a handleUserInput()

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 const { connect } = require("./client");
 // const stdin = process.stdin;
 // stdin.setEncoding("utf8");
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === "\u003") {
+    process.exit();
+  }
+};
+
 // stdin.on("data", (input) => {
 //   conn.write("finally successfully connected :)");
 
